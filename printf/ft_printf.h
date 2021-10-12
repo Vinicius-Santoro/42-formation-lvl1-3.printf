05bcfe9f1ceda7145686abd479d8b6d074018b59
Vinicius Naziozeno Santoro do Rio <83036509+Vinicius-Santoro@users.noreply.github.com>
2021-10-12 00:22:34 -0300

    printf finished

--- printf/ft_printf.h
+++ printf/ft_printf.h
@@ -17,19 +17,19 @@
 # include <unistd.h>
 # include <stdlib.h>
 
-int		  ft_printf(const char	*format, ...);
-int		  ft_type(va_list ap, char type);
+int		ft_printf(const char	*format, ...);
+int		ft_type(va_list ap, char type);
 
-int		  ft_type_char(int c);
-int		  ft_type_string(char *s);
-int		  ft_type_pointer(unsigned long p);
-int		  ft_type_decimal(int d);
-int		  ft_type_ux(unsigned int x, char	*base);
+int		ft_type_char(int c);
+int		ft_type_string(char *s);
+int		ft_type_pointer(unsigned long p);
+int		ft_type_decimal(int d);
+int		ft_type_ux(unsigned int x, char	*base);
 
 size_t	ft_strlen(const char *str);
 size_t	ft_strlcpy(char *destiny, const char *source, size_t destiny_size);
-char	  *ft_strjoin(char const *s1, char const *s2);
-char	  *ft_convert_base(size_t n, char *base);
-char	  *ft_uitoa(unsigned int unsigned_number);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_convert_base(size_t n, char *base);
+char	*ft_uitoa(unsigned int unsigned_number);
 
 #endif
